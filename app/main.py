60c534756a71ae29b58349c77af9189ad4a68a52
hspaces <support@hspace.biz>
2021-11-17 10:13:01 +0700

    update main.py

--- app/main.py
+++ app/main.py
@@ -20,7 +20,8 @@ from app.api_views.hello import *
 from fastapi_versioning import VersionedFastAPI
 app = VersionedFastAPI(
     app, version_format='{major}.{minor}',
-    prefix_format='/v{major}.{minor}'
+    prefix_format='/v{major}.{minor}',
+    enable_latest=True
 )
 
 
